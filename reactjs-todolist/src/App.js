4818efe30ec9ffa1b95aa9757d489138d8fbcb60
vuphan0804 <vuphan0804@gmail.com>
2021-05-18 16:05:21 +0700

    Buoc-1 todolist

--- reactjs-todolist/src/App.js
+++ reactjs-todolist/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { taskName: '', tasks: [] };
+	}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	myTaskChangeHandler = (event) => {
+		this.setState({ taskName: event.target.value });
+		// console.log('myTaskChangeHandler', event.target.value);
+	};
+
+	addTask = () => {
+		// console.log('addTask', this.state.taskName);
+		if (this.state.taskName === '') {
+			return;
+		}
+		this.state.tasks.push(this.state.taskName);
+		this.setState({ taskName: '' });
+	};
+
+	render() {
+		return (
+			<div className='App'>
+				<br />
+				Todo List
+				<br />
+				<div className='aligned'>
+					<img
+						src='./assets/iconfinder_gnome-app-install_29240 (1).png'
+						alt='Add Task'
+						width='25'
+						style={{ cursor: 'pointer' }}
+						title='Bấm để thêm task'
+						onClick={() => this.addTask()}
+					/>
+					<input
+						type='text'
+						value={this.state.taskName}
+						onChange={this.myTaskChangeHandler}
+					/>
+				</div>
+				<ul>
+					{this.state.tasks.map((value, index) => {
+						return <li key={index}>{value}</li>;
+					})}
+				</ul>
+			</div>
+		);
+	}
 }
 
 export default App;
